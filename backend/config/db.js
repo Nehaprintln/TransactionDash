6eb6d9e2168c472bd80fc44178a119fb8be18bec
Neha <ray.93gupta@gmail.com>
2024-12-24 02:18:46 +0530

    add database

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/TransactionsBoard', {
+    const conn = await mongoose.connect('mongodb+srv://ray93gupta:4xJAELEtc1n1KKtz@personalcluster0.myfou.mongodb.net/TransactionsBoard?retryWrites=true&w=majority&appName=PersonalCluster0', {
         // process.env.MONGO_URI
       useNewUrlParser: true,
       useUnifiedTopology: true,
